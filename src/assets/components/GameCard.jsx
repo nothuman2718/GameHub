ecc3ed66a4deb1e2a81994cf827c2a7914983aeb
nothuman2718 <nothuman2718@gmail.com>
2023-12-13 10:19:41 +0530

    Add Platform Icons

--- src/assets/components/GameCard.jsx
+++ src/assets/components/GameCard.jsx
@@ -1,6 +1,6 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
-
+import PlatformIconList from "./PlatformIconList";
 const GameCard = (props) => {
   const { game } = props;
   return (
@@ -8,6 +8,7 @@ const GameCard = (props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList game={game}></PlatformIconList>
       </CardBody>
     </Card>
   );
---
+++ src/assets/components/PlatformIconList.jsx
@@ -0,0 +1,35 @@
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPadding, MdPhoneIphone } from "react-icons/md";
+import { Text, Image, Icon, HStack } from "@chakra-ui/react";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+const PlatformIconList = (props) => {
+  const iconMap = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    mac: FaApple,
+    linux: FaLinux,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+  const { game } = props;
+  return (
+    <HStack marginY={1}>
+      {game.parent_platforms.map(({ platform }) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500"></Icon>
+      ))}{" "}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
