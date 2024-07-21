a74b173e08820f3667c85f7c68163377a1b21c29
nothuman <nothuman2.718@gmail.com>
2024-07-21 21:30:58 +0530

    Add dummy API Key

--- src/assets/services/apiClient.js
+++ src/assets/services/apiClient.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: ""
+        key: "69ce4a7dcc4144c58d90edea76f0b58c"
     }
 })
